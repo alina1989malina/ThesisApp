b6d9d1cdd325b5883a0b9eabf0fc80276256bf4b
alina <alina1989malina@yandex.ru>
2019-04-15 16:08:39 +0300

    formatting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,15 +6,15 @@ DROP TABLE IF EXISTS thesis;
 
 CREATE TABLE IF NOT EXISTS thesis (
 	id              SERIAL        NOT NULL  PRIMARY KEY,
-	thesis_title	  TEXT			    NOT NULL  UNIQUE,
-	pages_num		    INTEGER	      NOT NULL,
+	thesis_title	TEXT	      NOT NULL  UNIQUE,
+	pages_num		INTEGER	      NOT NULL,
 	supervisor_fio  VARCHAR(500)  NOT NULL,
 	aspirant_fio    VARCHAR(500)  NOT NULL,
- 	diss_def_time   DATE		NOT NULL,
-  science_code    VARCHAR(100)  NOT NULL,
-  science_field   VARCHAR(100)  NOT NULL,
-  org_title       TEXT          NOT NULL,
-  thesis_type     VARCHAR(100)  NOT NULL
+ 	diss_def_time   DATE		  NOT NULL,
+    science_code    VARCHAR(100)  NOT NULL,
+    science_field   VARCHAR(100)  NOT NULL,
+    org_title       TEXT          NOT NULL,
+    thesis_type     VARCHAR(100)  NOT NULL
 );
 
 -- Insert data
